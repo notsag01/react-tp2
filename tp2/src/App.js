0257252fb1db94bbe930bd91401bfd1fa3143d2c
notsag01 <gcordoba1981@gmail.com>
2021-12-08 00:08:00 -0300

    20211207A

--- tp2/src/App.js
+++ tp2/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Titulo} from "./componentes/titulo"; 
+import {Navbar} from "./componentes/navbar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Titulo />
+          <Navbar />
       </header>
     </div>
   );
---
+++ tp2/src/componentes/navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css"
+
+export const Navbar=()=>{
+    return(
+        <ul className="Navbar-pagina-principal">
+            <li>ORO</li>
+            <li>PLATA</li>
+            <li>ACERO</li>
+        </ul>
+    )
+}
