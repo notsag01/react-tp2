c7c979a5c3b01fe0e9a5a793d5eca15eeab45648
notsag01 <79101798+notsag01@users.noreply.github.com>
2021-12-17 15:18:37 -0300

    20211217.4

--- tp2/src/componentes/body-productos/items/index.jsx
+++ tp2/src/componentes/body-productos/items/index.jsx
@@ -1,15 +1,15 @@
-
+import React from "react"
+import styles from "../items/styles";
 
 
 
 const Item=({item})=>{
     return(
         <>
-            <div className="container" >
-                <div className="row">
+            <div className="container" style={styles} >
+                <div>
                     <h3> {item.nombre} </h3>
-                    <h4> ${item.precio} </h4>
-                    
+                    <h4> ${item.precio} </h4>                    
                 </div>
             </div>
         </>
---
+++ tp2/src/componentes/body-productos/items/styles.jsx
@@ -0,0 +1,7 @@
+const styles={
+    whidth:"30%",
+    color:"red"
+}
+
+
+export default styles
