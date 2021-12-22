f5d6b8260c2ed939c7f5ac525e39f19948535b5c
notsag01 <79101798+notsag01@users.noreply.github.com>
2021-12-22 13:55:59 -0300

    20211222.2

--- tp2/src/App.js
+++ tp2/src/App.js
@@ -6,7 +6,7 @@ import {
   Link
 } from "react-router-dom";
 import Navbar from "./componentes/Navbar";
-
+import ItemListContainer from "./componentes/ItemListContainer"
 
 function App() {
   return (
--- tp2/src/componentes/ItemListContainer.js
+++ tp2/src/componentes/ItemListContainer.js
@@ -6,6 +6,13 @@ import data from "../data/data"
 function ItemListContainer() {
     const [items, setItems]=useState([])
 
+    useEffect(()=>{
+        const promesaData= new Promise((resolve)=>{
+            resolve(data)
+        })
+
+        promesaData.then((res)=>setItems(res))
+    },[])
     return (
         <div>
             <ItemList/>
