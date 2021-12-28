94c55c41f3c428b8982492df4a06fe16761324e5
notsag01 <79101798+notsag01@users.noreply.github.com>
2021-12-28 17:32:50 -0300

    20211218.7

--- tp2/src/componentes/ItemDetail.js
+++ tp2/src/componentes/ItemDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 
-function ItemDetail({id, nombre}) {
+function ItemDetail({id, nombre, imagen, precio}) {
     return (
-        <div>
-            
-                <h1>{nombre}</h1>
-            
+        <div className="container">
+            <div className="row justify-content-center detail">
+                <div className="col-md-4">
+                    <img src={imagen} alt={id}></img>
+                </div>
+                <div className="col-md-4">
+                    <h1> {nombre} </h1>
+                    <h3> ${precio} </h3>
+                    <h6> ID:{id}</h6>
+                </div>
+            </div>
         </div>
     );
 }
