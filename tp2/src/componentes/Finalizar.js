f35f5a6286e63be2841cf47ee5aa63a15b0586d7
notsag01 <79101798+notsag01@users.noreply.github.com>
2022-01-26 17:00:46 -0300

    20220126.5

--- tp2/src/componentes/Finalizar.js
+++ tp2/src/componentes/Finalizar.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Finalizar() {
     return (
         <div>
-            <h1> FINAL </h1>
+            <div className="container">
+                <div className="row justify-content-center my-5">
+                    <h1> TU COMPRA FUE PROCESADA CON EXITO! </h1>                
+                    <h3> En breve, te llegará un mail de confirmación.</h3>                
+                </div>
+            </div>
         </div>
     );
 }
