6d64f60e748e6f088a4876453777826ecfcde038
notsag01 <79101798+notsag01@users.noreply.github.com>
2022-02-11 10:20:51 -0300

    20220211.1

--- tp2/src/componentes/ItemFormulario.js
+++ tp2/src/componentes/ItemFormulario.js
@@ -1,74 +1,23 @@
-import React, {useContext, useState } from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { CarritoContext } from "../context/CarritoContext";
-import {useForm} from 'react-hook-form'
+import React, {Fragment} from 'react'
+import { useForm } from 'react-hook-form'
 
-
-function ItemFormulario({onAdd}) {
-
-    const {clearCarrito}=useContext(CarritoContext)
-
-    const { register,errors, handleSubmit } = useForm();
+const ItemFormulario = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
 
     const onSubmit=(data)=>{
         console.log(data)
     }
-
-    const [datos, setDatos]=useState({
-        nombre:"",
-        apellido:"",
-        telefono:"",
-        email:""
-    })
-    
-
-    const handleChangeInput=(e)=>{
-        //console.log(e.target.value)
-        setDatos({
-            ...datos,
-            [e.target.name] : e.target.value
-        })
-    }
- 
+  
     return (
-        <div className="container formulario">            
-            <h1 className="row justify-content-center my-5">FORMULARIO</h1>
-            <div className="container">
-                <div className="col"></div>
-                <div className="col">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                            <input type="text" placeholder="Nombre" onChange={handleChangeInput} name="nombre"
-                            ref={register(
-                                'value.name',{
-                                required:{
-                                    value: true,
-                                    nessage:"Nombre requerido"
-                                }
-                            })}
-                            ></input>
-                            <span className="text-danger text-small d-block mb-2">
-                                {errors?.nombre?.message}
-                            </span>
-                        </div>
-                        <div>
-                            <input type="text" placeholder="Apellido" onChange={handleChangeInput} name="apellido"></input>
-                        </div>
-                        <div>
-                            <input type="tel" placeholder="Telefono" onChange={handleChangeInput} name="telefono"></input>
-                        </div>
-                        <div>
-                            <input type="mail" placeholder="Email" onChange={handleChangeInput} name="email"></input>
-                        </div>
-                        <Link to="/final"> <button type="submit" onClick={()=>{onAdd(datos); clearCarrito()}} >CONFIRMAR</button> </Link>
-                    </form>
-                </div>
-                <div className="col"></div>
-
-            </div>
-        </div>
-
-    );
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName", { required: true })} />
+        {errors.firstName?.type === 'required' && "First name is required"}
+        
+        <input {...register("lastName", { required: true })} />
+        {errors.lastName && "Last name is required"}
+        
+        <input type="submit" />
+      </form>)
 }
-
+ 
 export default ItemFormulario;
